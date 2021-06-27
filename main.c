ce129bc549486023e498c341192d4aaa5b02c60c
Seratooo <pauloserato2016@gmail.com>
2021-06-27 18:41:22 +0100

    Correcao de Bugs

--- main.c
+++ main.c
@@ -60,6 +60,8 @@ Rocha setRocha(int x,int y){
 
 int main(int argc, char *argv[]){
    
+  // int GER_PROC_COELHOS=2,GER_PROC_RAPOSAS=9,GER_ALIM_RAPOSAS=6,N_GER=4,N_OBJS,LIN=10,COL=10;
+
    int GER_PROC_COELHOS=2,GER_PROC_RAPOSAS=4,GER_ALIM_RAPOSAS=3,N_GER=6,N_OBJS,LIN=5,COL=5;
 
 /* printf("\nDigite o número de gerações até que um coelho possa procriar: ");
@@ -127,6 +129,204 @@ int main(int argc, char *argv[]){
     system("clear");
   }*/
 
+
+
+        /* Matriz[7][2].id=3;
+         Matriz[7][2].pedra = setRocha(7,2);
+
+
+         Matriz[0][1].id=3;
+         Matriz[0][1].pedra = setRocha(0,1);
+
+
+         Matriz[9][7].id=3;
+         Matriz[9][7].pedra = setRocha(9,7);
+
+
+         Matriz[2][7].id=3;
+         Matriz[2][7].pedra = setRocha(2,7);
+
+
+         Matriz[0][5].id=3;
+         Matriz[0][5].pedra = setRocha(0,5);
+
+
+         Matriz[2][5].id=3;
+         Matriz[2][5].pedra = setRocha(2,5);
+
+
+         Matriz[4][2].id=3;
+         Matriz[4][2].pedra = setRocha(4,2);
+
+
+         Matriz[6][8].id=3;
+         Matriz[6][8].pedra = setRocha(6,8);
+
+
+         Matriz[8][4].id=3;
+         Matriz[8][4].pedra = setRocha(8,4);
+
+
+         Matriz[1][1].id=3;
+         Matriz[1][1].pedra = setRocha(1,1);
+
+
+         Matriz[7][9].id=1;
+         Matriz[7][9].coelho = setCoelho(7,9,0,0);
+
+
+         Matriz[5][9].id=1;
+         Matriz[5][9].coelho = setCoelho(5,9,0,0);
+
+
+         Matriz[0][7].id=1;
+         Matriz[0][7].coelho = setCoelho(0,7,0,0);
+
+
+         Matriz[0][2].id=1;
+         Matriz[0][2].coelho = setCoelho(0,2,0,0);
+
+
+         Matriz[2][9].id=1;
+         Matriz[2][9].coelho = setCoelho(2,9,0,0);
+
+
+         Matriz[4][4].id=1;
+         Matriz[4][4].coelho = setCoelho(4,4,0,0);
+
+
+         Matriz[0][6].id=1;
+         Matriz[0][6].coelho = setCoelho(0,6,0,0);
+
+
+         Matriz[4][5].id=1;
+         Matriz[4][5].coelho = setCoelho(4,5,0,0);
+
+
+         Matriz[3][4].id=1;
+         Matriz[3][4].coelho = setCoelho(3,4,0,0);
+
+
+         Matriz[1][6].id=1;
+         Matriz[1][6].coelho = setCoelho(1,6,0,0);
+
+
+         Matriz[9][8].id=1;
+         Matriz[9][8].coelho = setCoelho(9,8,0,0);
+
+
+         Matriz[4][9].id=1;
+         Matriz[4][9].coelho = setCoelho(4,9,0,0);
+
+
+         Matriz[4][7].id=1;
+         Matriz[4][7].coelho = setCoelho(4,7,0,0);
+
+
+         Matriz[1][3].id=1;
+         Matriz[1][3].coelho = setCoelho(1,3,0,0);
+
+
+         Matriz[1][0].id=1;
+         Matriz[1][0].coelho = setCoelho(1,0,0,0);
+
+
+         Matriz[6][1].id=1;
+         Matriz[6][1].coelho = setCoelho(6,1,0,0);
+
+         Matriz[9][0].id=1;
+         Matriz[9][0].coelho = setCoelho(9,0,0,0);
+
+
+         Matriz[0][4].id=1;
+         Matriz[0][4].coelho = setCoelho(0,4,0,0);
+
+
+         Matriz[5][2].id=1;
+         Matriz[5][2].coelho = setCoelho(5,2,0,0);
+
+
+         Matriz[9][5].id=1;
+         Matriz[9][5].coelho = setCoelho(9,5,0,0);
+
+
+         Matriz[2][6].id=1;
+         Matriz[2][6].coelho = setCoelho(2,6,0,0);
+
+
+         Matriz[5][1].id=1;
+         Matriz[5][1].coelho = setCoelho(5,1,0,0);
+
+
+         Matriz[2][3].id=1;
+         Matriz[2][3].coelho = setCoelho(2,3,0,0);
+
+
+         Matriz[0][3].id=1;
+         Matriz[0][3].coelho = setCoelho(0,3,0,0);
+
+
+         Matriz[8][2].id=1;
+         Matriz[8][2].coelho = setCoelho(8,2,0,0);
+
+
+         Matriz[3][2].id=1;
+         Matriz[3][2].coelho = setCoelho(3,2,0,0);
+
+
+         Matriz[1][2].id=1;
+         Matriz[1][2].coelho = setCoelho(1,2,0,0);
+
+
+         Matriz[9][9].id=1;
+         Matriz[9][9].coelho = setCoelho(9,9,0,0);
+
+
+         Matriz[7][7].id=1;
+         Matriz[7][7].coelho = setCoelho(7,7,0,0);
+
+
+         Matriz[8][1].id=1;
+         Matriz[8][1].coelho = setCoelho(8,1,0,0);
+
+
+
+        
+         Matriz[4][3].id=2;
+         Matriz[4][3].raposa = setRaposa(4,3,0,0,0);
+                 
+         Matriz[3][8].id=2;
+         Matriz[3][8].raposa = setRaposa(3,8,0,0,0);
+
+                 
+         Matriz[5][4].id=2;
+         Matriz[5][4].raposa = setRaposa(5,4,0,0,0);
+        
+         Matriz[7][3].id=2;
+         Matriz[7][3].raposa = setRaposa(7,3,0,0,0);
+            
+         Matriz[6][0].id=2;
+         Matriz[6][0].raposa = setRaposa(6,0,0,0,0);
+                 
+         Matriz[9][2].id=2;
+         Matriz[9][2].raposa = setRaposa(9,2,0,0,0);
+        
+         Matriz[9][1].id=2;
+         Matriz[9][1].raposa = setRaposa(9,1,0,0,0);
+                 
+         Matriz[6][3].id=2;
+         Matriz[6][3].raposa = setRaposa(6,3,0,0,0);
+                 
+         Matriz[4][1].id=2;
+         Matriz[4][1].raposa = setRaposa(4,1,0,0,0);
+
+                 
+         Matriz[4][6].id=2;
+         Matriz[4][6].raposa = setRaposa(4,6,0,0,0);
+
+*/
+
+
          Matriz[0][0].id=3;
          Matriz[0][0].pedra = setRocha(0,0);
          
@@ -155,6 +355,11 @@ int main(int argc, char *argv[]){
          Matriz[4][4].raposa = setRaposa(4,4,0,0,0);
 
 
+
+
+
+         
+    
 
 
 
@@ -216,14 +421,16 @@ int main(int argc, char *argv[]){
 
                         int G =  Matriz[x][y].coelho.GER_PROC_COELHOS;
                         int newX,newY;
+                        newX = coordenadaPossivel[ (x+y+G)%Possibilidades].x;
+                        newY = coordenadaPossivel[ (x+y+G)%Possibilidades].y;
 
-                        if(Possibilidades>1){
+                        /*if(Possibilidades>1){
                               newX = coordenadaPossivel[ (x+y+G)%Possibilidades].x;
                               newY = coordenadaPossivel[ (x+y+G)%Possibilidades].y;
                         }else{
                                newX = coordenadaPossivel[0].x;
                                newY = coordenadaPossivel[0].y;
-                        }
+                        }*/
                         
                         // printf("CALULO -  (%d+%d+%d) mod %d = %d \n",x,y,G,Possibilidades,(x+y+G)%Possibilidades);
                         // printf("ANTIGO (%d,%d) ",x,y); 
@@ -259,6 +466,13 @@ int main(int argc, char *argv[]){
 
 
 
+                    }else{
+
+                            NewMatriz[x][y]=Matriz[x][y];
+                            
+                            NewMatriz[x][y].coelho.GER_PROC_COELHOS++;
+                            NewMatriz[x][y].coelho.idade++;
+                            Matriz[x][y].id = -2;
                     }
                        Possibilidades=0;
                   }
@@ -332,28 +546,28 @@ int main(int argc, char *argv[]){
                                 //VERIFICANDO POSSIVEIS PRESSAS A:
                                 //CIMA
                                 int ePresa=0;
-                                if(NewMatriz[x-1][y].id== 1){
+                                if(NewMatriz[x-1][y].id == 1 && x-1>=0 && y>=0 && x-1<=LIN-1 && y<=COL-1 && ePresa==0){
                                     newX = x-1;
                                     newY=y;
                                     printf("PRESA 1\n");
                                     ePresa=1;
                                 }
                                 //DIREITA
-                                if(NewMatriz[x][y+1].id== 1){
+                                if(NewMatriz[x][y+1].id == 1  && x>=0 && y+1>=0 && x<=LIN-1 && y+1<=COL-1){
                                     newY= y+1;
                                     newX=x;
                                     printf("PRESA 2\n");
                                     ePresa=1;
                                 }
                                 //BAIXO
-                                if(NewMatriz[x+1][y].id== 1){
+                                if(NewMatriz[x+1][y].id == 1 && x+1>=0 && y>=0 && x+1<=LIN-1 && y<=COL-1){
                                    newX=x+1;
                                    newY=y;
                                     printf("PRESA 3\n");
                                     ePresa=1;
                                 }
                                 //ESQUERDA
-                                if(NewMatriz[x][y-1].id== 1){
+                                if(NewMatriz[x][y-1].id == 1 && x>=0 && y-1>=0 && x<=LIN-1 && y-1<=COL-1){
                                   newY=y-1;
                                   newX=x;
                                   printf("PRESA 4\n");
@@ -372,7 +586,7 @@ int main(int argc, char *argv[]){
 
                             //MORRE SE NÃO SE ALIMENTAR
                             printf("MORTE ? : %d = %d | id %d\n",Matriz[x][y].raposa.GER_ALIM_RAPOSAS,GER_ALIM_RAPOSAS-1,Matriz[x][y].id);
-                            if(Matriz[x][y].raposa.GER_ALIM_RAPOSAS==GER_ALIM_RAPOSAS-1 && ePresa==0){
+                            if(Matriz[x][y].raposa.GER_ALIM_RAPOSAS>=GER_ALIM_RAPOSAS-1 && ePresa==0){
                              Matriz[x][y].id=-1;
                              NewMatriz[x][y].id=-1;
                              int nx = Matriz[x][y].raposa.coordenada.x;
@@ -406,7 +620,7 @@ int main(int argc, char *argv[]){
                                   NewMatriz[newX][newY].id=2;
 
                                   //ZERADOS NOVAMENTE
-                                  //NewMatriz[x][x].raposa.GER_PROC_RAPOSAS++;
+                                  NewMatriz[x][x].raposa.GER_PROC_RAPOSAS++;
                                   NewMatriz[x][y].raposa.idade=0; 
                                   //NewMatriz[newX][newY].raposa.GER_ALIM_RAPOSAS=0;
 
@@ -416,7 +630,15 @@ int main(int argc, char *argv[]){
                                   NewMatriz[x][y].id= -1;
                                 }
 
+                    }else{
+                            NewMatriz[x][y]=Matriz[x][y];
+                            
+                            NewMatriz[x][y].raposa.GER_PROC_RAPOSAS++;
+                            NewMatriz[x][y].raposa.idade++;
+                            NewMatriz[x][y].raposa.GER_ALIM_RAPOSAS++;
+                            Matriz[x][y].id = -2;
                     }
+
                        Possibilidades=0;
                   }
               }
@@ -432,9 +654,23 @@ int main(int argc, char *argv[]){
                    
                 }
               } 
-
-              printf("\n_______________________________________________________ \n");
-
+              printf("\n\nGERAÇÃO %d",k);
+              printf("\n____________________________________________________________________________________________________ \n");
+              for(int i=0;i<LIN;i++){
+              for(int j=0;j<COL;j++){
+                    if(Matriz[i][j].id==1){
+                        printf("| Coelho |");
+                    }
+                    else if(Matriz[i][j].id==2){
+                        printf("| Raposa |");
+                    }else if(Matriz[i][j].id==3){
+                      printf("|   *    |");
+                    }else{
+                      printf("|        |");
+                    }
+              }
+              printf("\n");
+            }
                  
 
 
@@ -442,7 +678,7 @@ int main(int argc, char *argv[]){
 
 
 
-            for(int i=0;i<LIN;i++){
+           /* for(int i=0;i<LIN;i++){
               for(int j=0;j<COL;j++){
                     if(NewMatriz[i][j].id==1){
                        printf("COELHOS (%d,%d)\n",i,j);
@@ -456,23 +692,23 @@ int main(int argc, char *argv[]){
                     }
               }
               printf("\n");
-            }
+            }*/
             
-            for(int i=0;i<LIN;i++){
+           /* for(int i=0;i<LIN;i++){
               for(int j=0;j<COL;j++){
-                    if(NewMatriz[i][j].id==1){
+                    if(Matriz[i][j].id==1){
                         printf("| Coelho |");
                     }
-                    else if(NewMatriz[i][j].id==2){
+                    else if(Matriz[i][j].id==2){
                         printf("| Raposa |");
-                    }else if(NewMatriz[i][j].id==3){
+                    }else if(Matriz[i][j].id==3){
                       printf("|   *    |");
                     }else{
                       printf("|        |");
                     }
               }
               printf("\n");
-            }
+            }*/
  
 
 
